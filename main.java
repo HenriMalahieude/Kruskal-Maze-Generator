96a0d41e27a82ae99a231a32baf2ed8a913a482f
Henri Malahieude <71425015+HenriMalahieude@users.noreply.github.com>
2021-09-30 15:26:51 -0700

    main file created

---
+++ main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Maze generator = new Maze(35); //input the size here
+    generator.newGeneration();
+    generator.printMaze();
+  }
+}
